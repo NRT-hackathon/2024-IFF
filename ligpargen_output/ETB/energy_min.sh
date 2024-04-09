932a1842cf718d14cd598838993ce0075404343f
Nikhil K <ikhilu95@gmail.com>
2024-04-09 19:36:29 -0400

    infiniate variations working check

--- ligpargen_output/ETB/energy_min.sh
+++ ligpargen_output/ETB/energy_min.sh
@@ -13,9 +13,9 @@ sed -i "10,\$ { /^$filename/s/.*/$filename         $copies/ }" "topol.top" # add
 
 # Energy minimization process
 gmx editconf -f "$filename.gro" -o edited_conf.gro -c -d 1.0 -bt cubic -box $boxlen
-gmx insert-molecules -f edited_conf.gro -nmol $copies -try 30 -ci "$filename.gro" -o edited_conf.gro
-gmx solvate -cp edited_conf.gro -cs spc216.gro -o solvated.gro -p topol.top
-gmx grompp -f minim.mdp -c solvated.gro -p topol.top -o em.tpr
-gmx mdrun -v -deffnm em
+#gmx insert-molecules -f edited_conf.gro -nmol $copies -try 30 -ci "$filename.gro" -o edited_conf.gro
+#gmx solvate -cp edited_conf.gro -cs spc216.gro -o solvated.gro -p topol.top
+#gmx grompp -f minim.mdp -c solvated.gro -p topol.top -o em.tpr
+#gmx mdrun -v -deffnm em
 
 
